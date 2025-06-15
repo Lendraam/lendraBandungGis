c4291e3feb81a3a52eb472ed9ea8b14c294268bd
Lendraam <148723086+Lendraam@users.noreply.github.com>
2025-06-15 17:19:42 +0700

    ADMINISTRASI_LN_25K_1_style.js

--- styles/ADMINISTRASI_LN_25K_1_style.js
+++ styles/ADMINISTRASI_LN_25K_1_style.js
@@ -21,7 +21,7 @@ var style_ADMINISTRASI_LN_25K_1 = function(feature, resolution){
         labelText = String("");
     }
     var style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(114,155,111,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.988}),
+        stroke: new ol.style.Stroke({color: 'rgba(114,100,60,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.988}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
